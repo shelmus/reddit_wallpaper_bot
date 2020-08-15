fe5d10515914879f929f6e78be4245cbf8267745
endorsystems <esane.demay@gmail.com>
2020-08-15 14:49:47 -0400

    Updated gitignore and cleaned up the files.

--- wallpaper.py
+++ wallpaper.py
@@ -10,19 +10,12 @@ def main():
                     user_agent = config.user_agent)
 
     subreddit = reddit.subreddit("wallpaper")
-    for submission in subreddit.stream.submissions(): #frskip_existing=True
-        os.system('wget --trust-server-names -P /home/sean/workspace/ripwalls/images/wallpaper/ {}'.format(submission.url))
-
-        # with open('/home/sean/workspace/personal/python/reddit_wallpaper/url.log', 'r+') as f:
-        #     for line in f.read():
-        #         if submission.url in line:
-        #             break
-        #         else: # not found
-        #             print >>f, submission.url
+    for submission in subreddit.stream.submissions(skip_existing=True): #frskip_existing=True
+        os.system('wget --trust-server-names -P /home/sean/documents/workspace/reddit_wallpaper_bot/images/wallpaper/ {}'.format(submission.url))
 
     subreddit = reddit.subreddit("EarthPorn")
     for submission in subreddit.stream.submissions(skip_existing=True):
-        os.system('wget --trust-server-names -P /home/sean/workspace/ripwalls/images/EarthPorn/ {}'.format(submission.url))
+        os.system('wget --trust-server-names -P /home/sean/documents/workspace/reddit_wallpaper_bot/images/EarthPorn/ {}'.format(submission.url))
 
 if __name__ == "__main__":
     main()
